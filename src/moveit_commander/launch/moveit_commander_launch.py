6316b0a37c729ff54c25abcb676df93db7155eda
Bachala Rajesh <bachala_rajesh@outlook.com>
2026-01-27 16:17:16 +0800

    feat: add commander that makes the robot moves with the help of moveit

---
+++ src/moveit_commander/launch/moveit_commander_launch.py
@@ -0,0 +1,16 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    config = os.path.join(
+        get_package_share_directory('moveit_commander'),
+        'config',
+        'moveit_commander_params.yaml',
+    )
+
+    return LaunchDescription([
+    ])
