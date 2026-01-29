bd7050d11a0e0d3102d28f2771676a5f3c7f8962
Bachala Rajesh <bachala_rajesh@outlook.com>
2026-01-29 12:26:24 +0800

    feat(teleop): add a teleop ros2 package for the so101 arm

---
+++ src/so101_teleop/setup.py
@@ -0,0 +1,34 @@
+import os
+from glob import glob
+from setuptools import find_packages, setup
+
+package_name = 'so101_teleop'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'config'), glob('config/*.yaml')),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='mira',
+    maintainer_email='todo@todo.todo',
+    description='teleoperation of so101 with joystick node',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'so101_teleop_node = so101_teleop.so101_teleop_node:main',
+        ],
+    },
+)
